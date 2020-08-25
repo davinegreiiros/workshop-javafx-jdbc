31d841075ea9fc7e067f33f281238523d368f04a
davinegreiiros <davi.negreiiros36@gmail.com>
2020-08-25 10:40:29 -0300

    Saving a new Department

--- src/gui/DepartmentFormController.java
+++ src/gui/DepartmentFormController.java
@@ -3,17 +3,26 @@ package gui;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import db.DbException;
+import gui.util.Alerts;
 import gui.util.Constraints;
+import gui.util.Utils;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import model.entities.Department;
+import model.service.DepartmentService;
 
 public class DepartmentFormController implements Initializable{
 
 	private Department entity;
+	
+	private DepartmentService service;
+		
 	
 	@FXML
 	private TextField txtId;
@@ -34,14 +43,40 @@ public class DepartmentFormController implements Initializable{
 		this.entity = entity;
 	}
 	
-	@FXML
-	public void onBtSaveAcction() {
-		System.out.println("onBtSaveAcction");
+	public void setDepartmentService(DepartmentService service) {
+		this.service = service;
 	}
 	
 	@FXML
-	public void onBtCancelAcction() {
-		System.out.println("onBtCancelAcction");
+	public void onBtSaveAcction(ActionEvent event ) {
+		if (entity == null) {
+			throw new IllegalStateException("Entity was null");
+		}
+		if (service == null) {
+			throw new IllegalStateException("Service was null");
+		}
+		try {
+			entity = getFormData();
+			service.saveOrUpdate(entity);
+			Utils.currentStage(event).close();
+		}
+		catch(DbException e) {
+			Alerts.showAlert("Error saving object", null, e.getMessage(), Alert.AlertType.ERROR);
+		}
+	}
+	
+	private Department getFormData() {
+		Department obj = new Department();
+		
+		obj.setId(Utils.tryParseToInt(txtId.getText()));
+		obj.setName(txtName.getText());
+		 
+		return obj;
+	}
+
+	@FXML
+	public void onBtCancelAcction(ActionEvent event) {
+		Utils.currentStage(event).close();
 	}
 	
 	
